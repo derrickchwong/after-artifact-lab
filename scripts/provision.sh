222664bf9f2c995f96b079d2bb0fd8dd8fa4df58
Mike Ensor <ensor@google.com>
2021-05-21 14:45:08 -0700

    installing docker as well

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -42,7 +42,7 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --role=roles/containeranalysis.notes.viewer
 
 # Install Kubectl (coupled to Ubuntu 2004-lts)
-snap install kubectl --classic
+snap install kubectl docker --classic
 
 ## Create GKE instance (used for deployment of container)
 
